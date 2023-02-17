009a36339a9007014a54bfc8f1fdab8f42c37ff3
Govind Kamtamneni <govind.k@microsoft.com>
2023-02-17 12:55:57 -0800

    add endpoint and usage examples

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # hotline-bling
-You used to call me
+
+A simple showcase of Azure OpenAI's capability to generate a summary of a pdf document.
+
+## Getting Started
+
+To get started, you'll need to have the following installed and provisioned:
+
+- [Python](https://www.python.org/downloads/)
+- [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
+- [Azure OpenAI](https://azure.microsoft.com/en-us/services/openai/)
+- [Docker](https://www.docker.com/)
+
+## Local Setup
+
+To get started, you'll need to create a `.env` file in the root of the project. This file will contain all of the environment variables that are used in the project. You can use the `.env.example` file as a template.
+
+1. Install requirements
+```bash
+pip install -r requirements.txt
+```
+
+2. Set the environment variables from `.env`
+
+3. Run the project locally
+
+```bash
+python -m uvicorn main:app --reload
+```
+4. Run using postman or curl
+
+```bash
+curl --location --request POST 'http://localhost:8000/tldr' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "paper_url": "https://www.sec.gov/files/form1-u.pdf"
+}'
+```
+![img.png](example.png)
+
+### Local Docker run
+
+To run the project locally using docker
+
+```bash
+docker-compose up
+```
---
+++ main.py
@@ -0,0 +1,87 @@
+import os
+import openai
+import wget
+import pathlib
+import pdfplumber
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseSettings, BaseModel
+
+
+class Settings(BaseSettings):
+    openai.api_type = "azure"
+    openai.api_base = os.getenv("OPENAI_API_BASE")
+    openai.api_version = "2022-06-01-preview"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+settings = Settings()
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Payload(BaseModel):
+    paper_url: str
+
+
+async def get_paper(paper_url):
+    """
+    Downloads a paper from it's arxiv page and returns
+    the local path to that file.
+    """
+    downloaded_paper = wget.download(paper_url)
+    downloaded_paper_file_path = pathlib.Path(downloaded_paper)
+
+    return downloaded_paper_file_path
+
+
+async def generate_tldr(content):
+    tldr_tag = "\n tl;dr:"
+
+    for page in content:
+        text = page.extract_text() + tldr_tag
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=text,
+            temperature=0.8,
+            max_tokens=140,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=["\n"]
+        )
+        print(f'GPT output: {response}')
+        return response["choices"][0]["text"]
+
+
+@app.get("/health")
+async def health():
+    return {"status": "Ok"}
+
+
+# Generate a tldr for a pdf paper
+@app.post("/tldr")
+async def tldr(req: Payload):
+    if not req.paper_url:
+        print("No paper url provided")
+        return {"error": "No paper_url provided"}
+
+    # Download the paper using wget
+    paper_file_path = await get_paper(req.paper_url)
+    print(f'Paper file path: {paper_file_path}')
+    paper_content = pdfplumber.open(paper_file_path).pages
+    print(f'Paper content: {paper_content}')
+
+    paper_tldr = await generate_tldr(paper_content)
+    print(f'Paper tldr: {paper_tldr}')
+    return paper_tldr
+
